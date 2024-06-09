5533f0553de442efdbe5e932585338ddadd303dd
quboqin <quboqin0710@gmail.com>
2024-06-09 13:40:59 +0800

    use local url

--- app/src/main/java/com/cosine/kidomo/ui/screen/home/HomeScreen.kt
+++ app/src/main/java/com/cosine/kidomo/ui/screen/home/HomeScreen.kt
@@ -1,11 +1,13 @@
 package com.cosine.kidomo.ui.screen.home
 
+import android.annotation.SuppressLint
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 import com.cosine.kidomo.ui.viewmodels.MainViewModel
 import com.cosine.kidomo.util.showToast
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun HomeScreen(
     gotoWebView: (taskId: Int) -> Unit,
--- app/src/main/java/com/cosine/kidomo/ui/screen/web/WebScreen.kt
+++ app/src/main/java/com/cosine/kidomo/ui/screen/web/WebScreen.kt
@@ -1,13 +1,20 @@
 package com.cosine.kidomo.ui.screen.web
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.util.Log
+import android.webkit.JavascriptInterface
+import android.webkit.WebChromeClient
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.Toast
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.viewinterop.AndroidView
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter", "SetJavaScriptEnabled")
 @Composable
 fun WebScreen() {
     Scaffold {
@@ -15,11 +22,25 @@ fun WebScreen() {
             factory = { context ->
                 WebView(context).apply {
                     webViewClient = WebViewClient()
+                    webChromeClient = WebChromeClient()
                     settings.javaScriptEnabled = true
-                    loadUrl("https://www.google.com")
+                    addJavascriptInterface(WebAppInterface(context), "Android")
+                    loadUrl("file:///android_asset/index.html")
                 }
             },
             modifier = Modifier.fillMaxSize()
         )
+    }
+}
+
+private class WebAppInterface(val context: Context) {
+    @JavascriptInterface
+    fun showToast(message: String) {
+        Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
+    }
+
+    @JavascriptInterface
+    fun logMessage(message: String) {
+        Log.d("WebAppInterface", message)
     }
 }
