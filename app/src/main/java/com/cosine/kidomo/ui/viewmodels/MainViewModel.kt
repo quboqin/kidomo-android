57a90dcb8dbe2f1099a40d747fb8b197f58e0496
quboqin <quboqin0710@gmail.com>
2024-06-15 09:30:37 +0800

    remove viewModelScope.launch

--- app/src/main/java/com/cosine/kidomo/ui/viewmodels/MainViewModel.kt
+++ app/src/main/java/com/cosine/kidomo/ui/viewmodels/MainViewModel.kt
@@ -14,27 +14,27 @@ class MainViewModel(
 
     val imageUri: MutableState<Uri> = mutableStateOf(EMPTY_IMAGE_URI)
 
-    fun updateImageUri(newImageUri: Uri)  {
+    fun updateImageUri(newImageUri: Uri) {
         imageUri.value = newImageUri
     }
 
     fun nativeTask(arg: Map<String, Any>, onBackButtonPressed: () -> Unit): String {
         var jsonString: String = ""
-        viewModelScope.launch {
-            val action = arg["action"] as? String
 
-            if (action == "send_credentials") {
-                // Assuming credentials have been sent successfully
-                val jsonObject = JSONObject()
-                jsonObject.put("key1", "value1")
-                jsonObject.put("key2", 12345)
-                jsonObject.put("key3", true)
+        val action = arg["action"] as? String
 
-                jsonString = jsonObject.toString()
-            } else if (action == "back") {
-                onBackButtonPressed()
-            }
+        if (action == "send_credentials") {
+            // Assuming credentials have been sent successfully
+            val jsonObject = JSONObject()
+            jsonObject.put("key1", "value1")
+            jsonObject.put("key2", 12345)
+            jsonObject.put("key3", true)
+
+            jsonString = jsonObject.toString()
+        } else if (action == "back") {
+            onBackButtonPressed()
         }
+
         return jsonString
     }
 
